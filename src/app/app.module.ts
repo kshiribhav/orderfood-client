7f889be89de6d31075fa0a2f08f82350030c39e8
Bhavna Kshirsagar <48829@hexaware.com>
2019-11-14 08:45:37 +0530

    Added libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AddhotelComponent } from './services/addhotel/addhotel.component';
+import { HomeComponent } from './components';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AddhotelComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
